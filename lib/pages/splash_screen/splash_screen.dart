5dc6c6c79eba39c085afebcc94451290d1f13b9a
piyushkarmacharya <piyushkarmacharya65@gmail.com>
2024-12-10 21:37:27 +0545

    add implicit animation in splash screen

--- lib/pages/splash_screen/splash_screen.dart
+++ lib/pages/splash_screen/splash_screen.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:marvel_app/pages/welcome_screen/welcome_screen.dart';
+import 'package:marvel_app/screen_size.dart';
 
 class SplashScreen extends StatefulWidget {
   static String routeName = "/splash-screen";
@@ -13,13 +14,13 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
-  Color bgColor = const Color(0xFFED1B24);
+  bool animate = false;
   @override
   void initState() {
     super.initState();
     Timer(const Duration(seconds: 3), () {
       setState(() {
-        bgColor = Colors.black;
+        animate = true;
       });
     });
     Timer(const Duration(seconds: 6), () {
@@ -30,8 +31,17 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: bgColor,
-      body: Center(child: SvgPicture.asset("assets/logo/Marvel-Logo.svg")),
+      backgroundColor: Colors.black,
+      body: Center(
+        child: AnimatedContainer(
+            duration: const Duration(milliseconds: 1000),
+            curve: Curves.bounceOut,
+            color: Theme.of(context).primaryColor,
+            height: animate ? 86 : ScreenSize.height,
+            width: animate ? 189 : ScreenSize.width,
+            child:
+                Center(child: SvgPicture.asset("assets/logo/Marvel-Logo.svg"))),
+      ),
     );
   }
 }
